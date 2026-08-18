aed893661d47fad1bf8ac9ecaeede4c93253dbde
Larissa <cyber.data@arancia.ca>
2026-08-18 10:43:03 -0300

    fix: desobstrucao da celula (0,1) no Nivel 3 permitindo movimentacao em escada (direita + baixo)

--- js/aula2-loopmaker.js
+++ js/aula2-loopmaker.js
@@ -363,9 +363,9 @@
             size: 5,
             start: { r:0, c:0 },
             chest: { r:4, c:4 },
-            walls: [ {r:0,c:1}, {r:0,c:2}, {r:0,c:3}, {r:1,c:0}, {r:2,c:0}, {r:3,c:0}, {r:2,c:1}, {r:3,c:2} ],
+            walls: [ {r:0,c:2}, {r:0,c:3}, {r:0,c:4}, {r:1,c:0}, {r:2,c:0}, {r:3,c:0}, {r:4,c:0}, {r:2,c:1}, {r:3,c:2}, {r:4,c:3} ],
             coins: [ {r:1,c:1}, {r:2,c:2}, {r:3,c:3} ],
-            objective: "🪙 Colete as 3 moedas em escada usando 1 único laço FOR com 2 comandos internos!",
+            objective: "🪙 Colete as 3 moedas no caminho em escada usando 1 laço FOR (direita + baixo)!",
             initialCode: ""
         },
         4: {
